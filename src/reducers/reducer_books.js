6b0a0b3787df597fbd623930fd8365eb859e85d1
D'ontreye Nero <dontreyenero@gmail.com>
2017-02-08 23:45:42 -0500

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        {'title': 'JS: The Good Parts'},
+        {'title': 'Vue'},
+        {'title': 'React'},
+        {'title': 'Angular'}
+    ];
+}
+
